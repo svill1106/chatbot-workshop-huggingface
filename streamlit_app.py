d0809612ec805d6891c90991caa2083951394762
svill1106 <sofia.villatoro@sjsu.edu>
2026-07-22 16:37:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def get_stopwords():
     nltk.download('stopwords')
 
 st.set_page_config(page_title="Chat with a friend about the AI Center", page_icon="🖥️", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with with a friend about the AI Centerfor Civic and Social Good ")
+st.title("Chat with with a friend about the AI Center for Civic and Social Good ")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
