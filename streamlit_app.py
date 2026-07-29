8ebe428753a6ddc74a8e9656c5dcfcda3c8f101b
svill1106 <sofia.villatoro@sjsu.edu>
2026-07-29 16:24:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,5 +137,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             message = {"role": "assistant", "content": response_stream.response}
             # Add response to message history
             st.session_state.messages.append(message)
-```
 
